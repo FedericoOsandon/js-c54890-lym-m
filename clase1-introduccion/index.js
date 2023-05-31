02ce27d01fe86b6709c7ace450a5d412ca551667
defe014@gmail.com <defe014@gmail.com>
2023-05-31 16:57:32 -0300

    Primer clase listo

---
+++ clase1-introduccion/index.js
@@ -0,0 +1,78 @@
+console.log('Hola Fede')
+
+
+// Palabras reservadas - Sintaxis
+// var - const - break - instanceof - typeof - if - 
+
+
+// variables y valores -  caja <- tipos de datos (string)
+// var   nombre   = 'Federico'
+// let   apellido = 'Osandón'
+// const CURSO    = "Javascrip"
+// const curso    = 'No es java'
+
+// Declaración 
+// let apellido
+// var nombre 
+// const curso 
+
+// var nombre_apellido 
+// var nombreApellidoAnio // camel case 
+// var NombreApellidoAnio // pascal case 
+// incorrecto
+// var nombre y apellido
+// var anio
+
+// Asignación
+// let edad = 30
+// inicializando variables
+// const edad1 = 30
+
+// reasignar 
+// edad = 35
+// edad1 = 35
+
+// no usar variables creadas con var
+
+// let numero = 1
+// let  numero2 = 2
+// const NUMERO3 = 3
+
+// let resultado      = numero + numero2 // es 1+2
+// let resultadoResta = numero - numero2 // 1 - 2
+// let multiplicacion = numero * NUMERO3
+
+
+// // operaciones básicas con string 
+// let   texto1 = 'Fede'
+// let   texto2 = 'Osandón'
+// const texto3 = ' '
+
+// let resultadoSuma = texto1 + texto2
+// let resultadoSuma1 = texto1 + 1
+// let resultadoSuma2 = texto1+texto3+texto2
+
+
+// prompt
+// let nombreIngresadoPrompt = prompt('Ingresar nombre, apura')
+// console.log(nombreIngresadoPrompt)
+
+// alert 
+// alert('Bienvenido a esta página')
+
+
+// ejemplo 
+let entrada = Number( prompt('Ingrese un Nombre: ') )
+console.log(typeof entrada)
+
+let salida  = entrada + 1
+
+console.log(salida)
+
+
+
+
+
+
+
+
